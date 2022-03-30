df26dc3e3af0c80aa59b3727c71c519508734dc3
danielalej <36351967+danielalej@users.noreply.github.com>
2022-03-30 14:00:26 -0500

    Create fuel_quote_history.php

---
+++ fuel_quote_history.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="stylesheet.css">
+	<title>Fuel Quote History</title>
+</head>
+<body>
+    <?php
+    //Include Config File
+    require_once "config.php";
+
+    //Initialize variables
+    $userID = "";
+
+    // WILL USE SESSIONS TO LOGIN 
+
+    ?>
+	<h1> Fuel Quote History </h1>
+
+    <h2> You are not logged in. <a href="login.php">Login now.</a></h2>
+
+	<table>
+		<tr>
+			<th> Gallons Requested </th>
+			<th> Delivery Address </th>
+			<th> Delivery Date </th>
+			<th> Suggested Price / gallon </th>
+			<th> Total Amount </th>
+		</tr>
+		<tr>
+			<td> 53 </td>
+			<td> Apple Street
+			New York, 12345 </td>
+			<td> 11/21/2021 </td>
+			<td> $2.56 </td>
+			<td> $135.68 </td>
+		</tr>
+		<tr>
+			<td> 53 </td>
+			<td> Apple Street
+			New York, 12345 </td>
+			<td> 11/21/2021 </td>
+			<td> $2.56 </td>
+			<td> $135.68 </td>
+		</tr>
+		<tr>
+			<td> 53 </td>
+			<td> Apple Street
+			New York, 12345 </td>
+			<td> 11/21/2021 </td>
+			<td> $2.56 </td>
+			<td> $135.68 </td>
+		</tr>
+	</table>
+</body>
+</html>
