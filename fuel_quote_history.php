b868dd41a209fba6f0da4bced0227344db7c7aae
danielalej <36351967+danielalej@users.noreply.github.com>
2022-04-01 17:22:17 -0500

    Update fuel_quote_history.php
    Added code to display table data from database

--- fuel_quote_history.php
+++ fuel_quote_history.php
@@ -20,6 +20,15 @@
 		$style = "style='background-color:red;'";
 	}
 	
+    // WILL USE SESSIONS TO LOGIN 
+	
+    ?>
+	<h1> Fuel Quote History </h1>
+
+    <h2 <?php echo $style;?>> You are not logged in. <a href="index.html">Login now.</a></h2>
+
+	<?php
+
     // Attempt to connect to database FuelQuote
 	$connection = new mysqli("localhost", "root", "", "FuelQuote");
 
@@ -30,27 +39,8 @@
 	
 	$sql = "SELECT UserID, Gallons, DeliveryAddress, DeliveryDate, SuggestedPrice, Total FROM FuelQuoteTable";
 	$result = $connection->query($sql);
-	
-	// Go through all rows in table
-	if ($result->num_rows > 0) {
-		// output all rows
-		while ($row = $result->fetch_assoc()) {
-			if ($row['UserID'] == $userID) {
-			
-			}
-		}
-	} else {
-		echo "empty table";
-	}
 
-
-    // WILL USE SESSIONS TO LOGIN 
-	
-    ?>
-	<h1> Fuel Quote History </h1>
-
-    <h2 <?php echo $style;?>> You are not logged in. <a href="index.html">Login now.</a></h2>
-
+	echo "
 	<table>
 		<tr>
 			<th> Gallons Requested </th>
@@ -58,31 +48,26 @@
 			<th> Delivery Date </th>
 			<th> Suggested Price / gallon </th>
 			<th> Total Amount </th>
-		</tr>
-		<tr>
-			<td> 53 </td>
-			<td> Apple Street
-			New York, 12345 </td>
-			<td> 11/21/2021 </td>
-			<td> $2.56 </td>
-			<td> $135.68 </td>
-		</tr>
-		<tr>
-			<td> 53 </td>
-			<td> Apple Street
-			New York, 12345 </td>
-			<td> 11/21/2021 </td>
-			<td> $2.56 </td>
-			<td> $135.68 </td>
-		</tr>
-		<tr>
-			<td> 53 </td>
-			<td> Apple Street
-			New York, 12345 </td>
-			<td> 11/21/2021 </td>
-			<td> $2.56 </td>
-			<td> $135.68 </td>
-		</tr>
-	</table>
+		</tr>";
+
+	// Go through all rows in table
+	if ($result->num_rows > 0) {
+		// output all rows
+		while ($row = $result->fetch_assoc()) {
+			if ($row['UserID'] == $userID) {
+				echo "<tr>";
+				echo "<td>" . $row['Gallons'] . "</td>";
+				echo "<td>" . $row['DeliveryAddress'] . "</td>";
+				echo "<td>" . $row['DeliveryDate'] . "</td>";
+				echo "<td>" . $row['SuggestedPrice'] . "</td>";
+				echo "<td>" . $row['Total'] . "</td>";
+			}
+		}
+	} else {
+		echo "NOT LOGGED IN";
+	}
+
+	?>
+
 </body>
 </html>
